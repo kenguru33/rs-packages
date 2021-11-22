716b28ab9c948c1657471346d51bbc63a6f7dda4
kenguru33 <bernt.anker@rs.no>
2021-11-22 10:53:21 +0100

    prettify

--- packages/test-app/src/app.module.ts
+++ packages/test-app/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NatsJetStreamTransport } from '@nestjs-plugins/nats-jetstream-transport'
+import { NatsJetStreamTransport } from '@nestjs-plugins/nats-jetstream-transport';
 
 @Module({
   imports: [
     NatsJetStreamTransport.register({
       connectionOptions: {
-        servers: 'localhost:4222'
+        servers: 'localhost:4222',
       },
       streamConfig: {
-        subjects: ['order.*']
-      }
-    })
+        subjects: ['order.*'],
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
